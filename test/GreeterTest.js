90dd8af1d287d18f1ffc0a3156a1b2c0a753c35f
Silverbrottle <40502339+Silverbrottle@users.noreply.github.com>
2020-07-20 16:30:12 +0530

    Update GreeterTest.js

--- test/GreeterTest.js
+++ test/GreeterTest.js
@@ -1,19 +1,19 @@
-const GreeterContract = artifacts.require('Greeter');
+const GreeterContract = artifacts.require("Greeter");
 contract(
-    'Greeter',
-    () => {
-        it('Contract has been deployed!', async function() {
-            const greeter = await GreeterContract.deployed();
-            assert(greeter, "contract wasn't deployed");
-        });
-    }
-    ,
-    describe('greet()', () => {
-        it("returns Good Evening", async() => {
-            const greeter = await GreeterContract.deployed();
-            const expected = "Good Evening";
-            const actual = await greeter.greet();
-            assert.equal(actual, expected, "Greeting Should be 'Good Evening'");
-        });
-    })
+  "Greeter",
+  () => {
+    it("Contract has been deployed!", async function () {
+      const greeter = await GreeterContract.deployed();
+      assert(greeter, "contract wasn't deployed");
+    });
+  },
+  describe("greet()", () => {
+    it("returns Good Evening", async () => {
+      const greeter = await GreeterContract.deployed();
+      const expected = "Good Evening";
+      // const expected = "";
+      const actual = await greeter.greet();
+      assert.equal(actual, expected, "Greeting Should be 'Good Evening'");
+    });
+  })
 );

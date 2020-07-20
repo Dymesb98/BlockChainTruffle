60b1508a008056e2dae9f7ac9fd1210ad4c030ee
Silverbrottle <40502339+Silverbrottle@users.noreply.github.com>
2020-07-20 16:11:40 +0530

    Update GreeterTest.js

--- test/GreeterTest.js
+++ test/GreeterTest.js
@@ -7,14 +7,13 @@ contract(
             assert(greeter, "contract wasn't deployed");
         });
     }
-    /*
     ,
     describe('greet()', () => {
         it("returns 'Good Evening'", async() => {
             const greeter = await GreeterContract.deployed();
-            const expected = "'Good Evening'";
+            const expected = "Good Evening";
             const actual = await greeter.greet();
             assert.equal(actual, expected, "Greeting Should be 'Good Evening'");
         });
-    })*/
+    });
 );
